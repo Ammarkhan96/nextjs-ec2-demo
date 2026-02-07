c41e69536940a15a972f4e0ec0bd8f2a92d34037
Ammarkhan96 <ammarkhaan96@gmail.com>
2026-02-07 17:08:44 +0500

    Update home page content

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main style={{ padding: "40px" }}>
       <h1>Next.js CI/CD Demo 🚀</h1>
       <p>Auto deployed using GitHub Actions</p>
-      <p>Time: {new Date().toLocaleString()}</p>
+      <p>This is demo Deployment through EC2 instance</p>
     </main>
   );
 }
